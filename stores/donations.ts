d1101242f230e8d290eac8c1d0cb0a6e43ea44e2
JonathanCouck-weglow <jonathan.couck@weglow.world>
2025-02-19 00:49:12 +0100

    Fix shit

--- stores/donations.ts
+++ stores/donations.ts
@@ -17,7 +17,7 @@ export const useDonationsStore = defineStore('donationsStore', {
         const result = await $fetch<{
           messages: Message[];
           coords?: Coord[];
-        }>(`http://localhost:8000/api/campaign/donations/${orgSlug}/${campaignSlug}`, {
+        }>(`https://weglow-backend.azurewebsites.net/api/campaign/donations/${orgSlug}/${campaignSlug}`, {
           query: {
             limit: 20,
             offset: this.page * 20,
