d29d1fbf54fe82efc96bd6e15ef5baa7a4edec06
JonathanCouck-weglow <jonathan.couck@weglow.world>
2025-03-26 10:52:25 +0100

    Testing

--- pages/o/[orgSlug]/c/[campaignSlug].vue
+++ pages/o/[orgSlug]/c/[campaignSlug].vue
@@ -11,7 +11,6 @@
 </template>
 
 <script lang="ts">
-// import { convert } from 'html-to-text';
 import { Template, VarType } from '~/models/enums';
 
 export default defineComponent({
--- pages/testing.vue
+++ pages/testing.vue
@@ -5,15 +5,27 @@
 </template>
 
 <script lang="ts" setup>
-import type { Content } from '~/models/content';
+import { convert } from 'html-to-text';
+import { VarRefType, VarType } from '~/models/enums';
 
-const content = await useApiTest<Content>(`content-4H3OBDBO`, '/campaign/content/feestvarken-vzw/4H3OBDBO');
+const campaignStore = useCampaignStore();
+await campaignStore.init();
+const content = campaignStore.content;
+// const content = await useApiTest<Content>(`content-4H3OBDBO`, '/campaign/content/feestvarken-vzw/4H3OBDBO');
 definePageMeta({
   layout: 'testing',
 });
 if (content) {
-  useServerSeoMeta({
-    title: content.org.slug,
-  });
+  const title = campaignStore.variable('campaign_name', campaignStore.locale) || campaignStore.variable('org_name', campaignStore.locale);
+  const description = campaignStore.variable('description', campaignStore.locale, VarType.TRANSLATION, VarRefType.CAMPAIGN)
+    || campaignStore.variable('description', campaignStore.locale, VarType.TRANSLATION, VarRefType.ORG) || '';
+  const image = campaignStore.variable('description', campaignStore.locale, VarType.IMAGE, VarRefType.CAMPAIGN)
+    || campaignStore.variable('logo', campaignStore.locale, VarType.IMAGE, VarRefType.ORG);
+
+  useServerSeoMeta(varsToSeo({
+    title,
+    description: convert(description),
+    image,
+  }));
 }
 </script>
